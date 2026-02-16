82edf3e7466cc3428f759aac5bdcc29b8c390258
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-02-16 19:57:06 +0700

    process of resolving merge conflicts

--- backend/internal/infrastructure/postgres/creation.go
+++ backend/internal/infrastructure/postgres/creation.go
@@ -6,18 +6,10 @@ import (
 	"os"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 )
 
-type PgxConn interface {
-	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
-	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
-	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
-	Close(ctx context.Context) error
-}
-
 type Postgres struct {
-	conn    PgxConn
+	conn    *pgx.Conn
 	errChan chan<- error
 }
 
